58dc57df6f6d88841d8d962bef6f9a732e1da95b
nandor matyas <nandor.matyas@gmail.com>
2019-01-09 16:08:04 +0100

    feat(react): route parameters

--- src/App.js
+++ src/App.js
@@ -4,17 +4,17 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
-
+import Post from './components/Post';
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <div className="App">
           <Navbar />
-          <Route exact path='/' component={Home}/>
-          <Route path='/about' component={About}/>
-          <Route path='/contact' component={Contact}/>
-
+          <Route exact path='/' component={Home} />
+          <Route path='/about' component={About} />
+          <Route path='/contact' component={Contact} />
+          <Route path="/:post_id" component={Post} />
         </div>
       </BrowserRouter>
     );
