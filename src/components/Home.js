6afbf750ff544c9ba6c6d50557149bb639fad0f0
nandor matyas <nandor.matyas@gmail.com>
2019-01-14 13:59:38 +0100

    feat(react):redux added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,22 +1,23 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+//import axios from 'axios';
 import { Link } from 'react-router-dom';
 import Pokeball from '../pokeball.png';
-
+import { connect } from 'react-redux';
 class Home extends Component {
-  state = {
-    posts: [],
-  }
-  componentDidMount(){
-    axios.get('https://jsonplaceholder.typicode.com/posts')
-      .then(res => {
-        this.setState({
-          posts: res.data.slice(0, 10)
-        })
-      })
-  }
+  // state = {
+  //   posts: [],
+  // }
+  // componentDidMount(){
+  //   axios.get('https://jsonplaceholder.typicode.com/posts')
+  //     .then(res => {
+  //       this.setState({
+  //         posts: res.data.slice(0, 10)
+  //       })
+  //     })
+  // }
+
   render() {
-    const { posts } = this.state;
+    const { posts } = this.props;
     const postList = posts.length ? (
       posts.map(post => {
         return (
@@ -43,4 +44,10 @@ class Home extends Component {
   }
 }
 
-export default Home
+const mapStateToProps = (state) => {
+  return {
+    posts: state.posts
+  }
+}
+
+export default connect(mapStateToProps)(Home)
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ const Navbar = (props) => {
   return (
     <nav className="nav-wrapper red darken-3">
         <div className="container">
-            <a href="" className="brand-logo">Nandi Times</a>
+            <a href="" className="brand-logo">Poke Times</a>
             <ul className="right">
               <li><Link to="/">Home</Link></li>
               <li><NavLink to="/about">About</NavLink></li>
